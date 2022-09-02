13300b2e1b283399e27d524b3efd4696d8a76b1a
kritikaapathak <109302179+kritikaapathak@users.noreply.github.com>
2022-09-02 18:29:10 +0530

    Add files via upload

---
+++ Magic ball88.py
@@ -0,0 +1,64 @@
+import time
+import random
+print("your welcome!! In the world of magic")
+print(" ")
+print("Hey!, what is your name?")
+name = input()
+print("hello" + name)
+print()
+
+input("Ask your question")
+time.sleep(2)
+print()
+print("Wait for your answer...........")
+print()
+
+time.sleep(2)
+user_response = random.randint(0,20)
+if(user_response ==1):
+    print("YEAH SURE!!")
+elif(user_response ==2):
+      print("Better luck next time")
+elif(user_response ==3):
+      print("NO")
+elif(user_response ==3):
+     print("May be not")
+elif(user_response ==4):
+     print("Universe says No")
+elif(user_response ==5):
+     print("Can not say")
+elif(user_response ==6):
+     print("YES")
+elif(user_response ==7):
+     print("Pure energies saying yes")
+elif(user_response ==8):
+     print("May be")
+elif(user_response ==9):
+     print("it is certain")
+elif(user_response ==10):
+     print("You may rely on it")
+elif(user_response ==11):
+     print("My sources says no") 
+elif(user_response ==12):
+     print("Ask your question again")
+elif(user_response ==13):
+     print("My sources says yes")
+elif(user_response ==14):
+     print("Why not")
+elif(user_response ==15):
+     print("For sure")
+elif(user_response ==16):
+     print("Yes! why not?")
+elif(user_response ==17):
+     print("You can")
+elif(user_response ==17):
+     print("Concentrate! and ask again")
+elif(user_response ==19):
+     print("Most likely")
+elif(user_response ==20):
+     print("As i see it, is no")
+else:
+    print("Question is invalid")
+print()
+print()
+print("THANK YOU!!")
